0e07a40b145628fd8bb765eb6c0edb90acbc86ec
yangshaobooo <1783494374@qq.com>
2024-06-12 13:31:59 +0800

    alter

--- controller/user.go
+++ controller/user.go
@@ -11,10 +11,13 @@ import (
 
 // Login 用户登录函数
 func Login(c *gin.Context) {
-	// 1、获取前端传递的account和password
+	// 1、获取前端传递的username和password
 	p := new(models.ReqLogin)
+	resp := new(models.Response)
 	if err := c.ShouldBind(p); err != nil { // 不同的参数发送方式，使用不同的绑定方式
 		zap.L().Error("Login with invalid param", zap.Error(err))
+		resp.StatusMsg = "请求参数错误"
+		c.JSON(400, resp)
 		return
 	}
 	fmt.Println("登录的用户", p)
@@ -22,9 +25,11 @@ func Login(c *gin.Context) {
 	res, err := logic.Login(p)
 	if err != nil {
 		zap.L().Error("logic.Login() error", zap.Error(err))
+		resp.StatusMsg = "账号密码错误"
+		c.JSON(400, resp)
 		return
 	} else {
-		zap.L().Info("用户登录成功", zap.String("account", p.Username))
+		zap.L().Info("用户登录成功", zap.String("username", p.Username))
 	}
 	// 3、返回响应
 	c.JSON(http.StatusOK, res)
